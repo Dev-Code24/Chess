1821721bfba32c9dc7c8e9360ef6efb88df1f065
Dev Code <hellodev_code@outlook.com>
2025-03-24 10:37:15 +0530

    initiated sidenav feature

---
+++ Chess-frontend/src/app/sidenav/utils/sidenav-button.ts
@@ -0,0 +1,31 @@
+export interface sidenavButtonsInterface {
+   img: string;
+   title: string;
+};
+
+export const sidenavButtons: sidenavButtonsInterface[] = [
+   {
+      img: '',
+      title: 'Play',
+   },
+   {
+      img: '',
+      title: 'Puzzles',
+   },
+   {
+      img: '',
+      title: 'Learn',
+   },
+   {
+      img: '',
+      title: 'Watch',
+   },
+   {
+      img: '',
+      title: 'News',
+   },
+   {
+      img: '',
+      title: 'Social',
+   },
+];
