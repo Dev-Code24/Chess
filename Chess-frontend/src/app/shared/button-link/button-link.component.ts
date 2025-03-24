5b4625baa2d1d3083d1e917cdd9f21cab96cd007
Dev Code <hellodev_code@outlook.com>
2025-03-24 10:33:52 +0530

    initiated required file structure

---
+++ Chess-frontend/src/app/shared/button-link/button-link.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-link',
+  imports: [],
+  templateUrl: './button-link.component.html',
+  styleUrl: './button-link.component.scss'
+})
+export class ButtonLinkComponent {
+}
