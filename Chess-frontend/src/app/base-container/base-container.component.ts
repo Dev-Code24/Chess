e20880cb0006d36578addc8599e2cbae8d95ad47
Dev Code <hellodev_code@outlook.com>
2025-04-07 21:42:17 +0530

    created basic directory layout

---
+++ Chess-frontend/src/app/base-container/base-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-container',
+  imports: [],
+  templateUrl: './base-container.component.html',
+  styleUrl: './base-container.component.scss'
+})
+export class BaseContainerComponent {
+
+}
---
+++ Chess-frontend/src/app/base-sidebar/base-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-sidebar',
+  imports: [],
+  templateUrl: './base-sidebar.component.html',
+  styleUrl: './base-sidebar.component.scss'
+})
+export class BaseSidebarComponent {
+
+}
